cb1c74ea8c8bd9b74ef1795664c7d9c184353536
Tuomas Siipola <siiptuo@kapsi.fi>
2016-01-31 01:04:49 +0200

    Add basic tuning selection

--- bundle.js
+++ bundle.js
@@ -8,15 +8,16 @@ const scales = {
     'locrian': [0, 1, 3, 5, 6, 8, 10]
 };
 const notes = ['C', 'C♯', 'D', 'D♯', 'E', 'F', 'F♯', 'G', 'G♯', 'A', 'A♯', 'B'];
-const tuning = [4, 9, 2, 7, 11, 4];
+const notes2 = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
+
+function parseTuning(input) {
+    return input.split('').map(i => notes2.indexOf(i));
+}
 
 const fretboard = document.body.appendChild(document.createElement('div'));
 fretboard.className = 'fretboard';
 
 const noteFretMap = new Array(12);
-for (let i = 0; i < 12; i++) {
-    noteFretMap[i] = []
-}
 
 const frets = new Array(25);
 frets[0] = 50;
@@ -26,26 +27,6 @@ for (let i = 1; i < 25; i++) {
     max += frets[i];
 }
 max /= 100;
-for (let i = 0; i < 25; i++) {
-    const fret = fretboard.appendChild(document.createElement('div'));
-    fret.className = 'fretboard-fret';
-    fret.style.width = (frets[i] / max) + '%';
-
-    for (let j = 5; j >= 0; j--) {
-        const fret2 = fret.appendChild(document.createElement('div'));
-        const noteIndex = (tuning[j] + i) % 12;
-        const note = notes[noteIndex];
-        fret2.className = 'fretboard-fret2';
-
-        const text = fret2.appendChild(document.createElement('span'));
-        text.className = 'fretboard-fret-text';
-        text.textContent = note;
-        noteFretMap[noteIndex].push(fret2);
-    }
-
-    const number = fret.appendChild(document.createElement('div'));
-    number.textContent = i;
-}
 
 function changeScale(scale, key) {
     for (let fret of document.getElementsByClassName('fretboard-fret2')) {
@@ -55,6 +36,34 @@ function changeScale(scale, key) {
         for (let fret of noteFretMap[(key + scale[i]) % 12]) {
             fret.classList.add('selected');
         }
+    }
+}
+
+function changeTuning(tuning) {
+    fretboard.innerHTML = '';
+    for (let i = 0; i < 12; i++) {
+        noteFretMap[i] = [];
+    }
+
+    for (let i = 0; i < 25; i++) {
+        const fret = fretboard.appendChild(document.createElement('div'));
+        fret.className = 'fretboard-fret';
+        fret.style.width = (frets[i] / max) + '%';
+
+        for (let j = tuning.length - 1; j >= 0; j--) {
+            const fret2 = fret.appendChild(document.createElement('div'));
+            const noteIndex = (tuning[j] + i) % 12;
+            const note = notes[noteIndex];
+            fret2.className = 'fretboard-fret2';
+
+            const text = fret2.appendChild(document.createElement('span'));
+            text.className = 'fretboard-fret-text';
+            text.textContent = note;
+            noteFretMap[noteIndex].push(fret2);
+        }
+
+        const number = fret.appendChild(document.createElement('div'));
+        number.textContent = i;
     }
 }
 
@@ -68,4 +77,11 @@ scaleSelect.addEventListener('change', event => {
     changeScale(scales[scaleSelect.value], +keySelect.value);
 });
 
+const tuningSelect = document.getElementsByName('tuning')[0];
+tuningSelect.addEventListener('change', event => {
+    changeTuning(parseTuning(tuningSelect.value));
+    changeScale(scales[scaleSelect.value], +keySelect.value);
+});
+
+changeTuning(parseTuning(tuningSelect.value));
 changeScale(scales[scaleSelect.value], +keySelect.value);
