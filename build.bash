ad4cfcfc4220d9856836e116b27f324ef6c301d4
Tuomas Siipola <siiptuo@kapsi.fi>
2018-07-15 17:20:27 +0300

    Optimize images and minify CSS

--- build.bash
+++ build.bash
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
+export PATH=$PATH:node_modules/.bin
 
 ffmpeg() {
     if hash avconv 2>/dev/null; then
@@ -9,13 +10,25 @@ ffmpeg() {
     fi
 }
 
-mkdir -p public
-cp -r logo.svg fretboard.png style.css index.html chords.html public
+mkdir -p public/images
+cp index.html chords.html public
 
+# Minify CSS
+cleancss -o public/style.css style.css
+
+# Compile and minify JavaScript
+babel ./*.js --out-dir public
+minify public/*.js --out-dir public
+
+# Optimize images
+convert -quality 100 images/fretboard.png public/images/fretboard.jpeg
+jpegoptim -m50 public/images/fretboard.jpeg
+sed -i -- 's/fretboard\.png/fretboard\.jpeg/g' public/*.css
+
+svgo --input=images/logo.svg --output=public/images/logo.svg
+
+# Optimize sounds
 mkdir -p public/sounds
 for sound in sounds/*; do
   ffmpeg -y -loglevel warning -i "$sound" -ac 1 -c pcm_s16le "public/$sound"
 done
-
-./node_modules/.bin/babel ./*.js --out-dir public
-./node_modules/.bin/minify public/*.js --out-dir public
