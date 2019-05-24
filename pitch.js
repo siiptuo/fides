95b5c1d71e71e21cc09f875cf94bfa72fd8434af
Tuomas Siipola <siiptuo@kapsi.fi>
2019-05-24 11:26:42 +0300

    Split code into multiple files

---
+++ pitch.js
@@ -0,0 +1,87 @@
+const alphabet = ["C", "D", "E", "F", "G", "A", "B"];
+const alphabetPitches = [0, 2, 4, 5, 7, 9, 11];
+const notes = [
+  ["B♯", "C", "D♭♭"],
+  ["B♯♯", "C♯", "D♭"],
+  ["C♯♯", "D", "E♭♭"],
+  ["D♯", "E♭", "F♭♭"],
+  ["D♯♯", "E", "F♭"],
+  ["E♯", "F", "G♭♭"],
+  ["E♯♯", "F♯", "G♭"],
+  ["F♯♯", "G", "A♭♭"],
+  ["G♯", "A♭"],
+  ["G♯♯", "A", "B♭♭"],
+  ["A♯", "B♭", "C♭♭"],
+  ["A♯♯", "B", "C♭"]
+];
+
+export class Pitch {
+  static listPitchClass(pitchClass) {
+    return notes[pitchClass].map(n => new Pitch(n));
+  }
+
+  constructor(name) {
+    this.letter = alphabet.indexOf(name[0]);
+    if (name.length === 1) {
+      this.modifier = 0;
+    } else if (name.length === 2) {
+      if (name[1] === "♯") {
+        this.modifier = 1;
+      } else if (name[1] === "♭") {
+        this.modifier = -1;
+      }
+    } else if (name.length == 3) {
+      if (name[1] === "♯" && name[2] === "♯") {
+        this.modifier = 2;
+      } else if (name[1] === "♭" && name[2] === "♭") {
+        this.modifier = -2;
+      }
+    }
+  }
+
+  isDoubleFlat() {
+    return this.modifier === -2;
+  }
+
+  isFlat() {
+    return this.modifier === -1;
+  }
+
+  isNatural() {
+    return this.modifier === 0;
+  }
+
+  isSharp() {
+    return this.modifier === 1;
+  }
+
+  isDoubleSharp() {
+    return this.modifier === 2;
+  }
+
+  getLetter() {
+    return this.letter;
+  }
+
+  getPitchClass() {
+    return (alphabetPitches[this.letter] + this.modifier) % 12;
+  }
+
+  toString() {
+    if (this.isDoubleFlat()) {
+      return alphabet[this.letter] + "♭♭";
+    }
+    if (this.isFlat()) {
+      return alphabet[this.letter] + "♭";
+    }
+    if (this.isNatural()) {
+      return alphabet[this.letter];
+    }
+    if (this.isSharp()) {
+      return alphabet[this.letter] + "♯";
+    }
+    if (this.isDoubleSharp()) {
+      return alphabet[this.letter] + "♯♯";
+    }
+  }
+}
---
+++ scale.js
@@ -0,0 +1,134 @@
+import { Pitch } from './pitch.js';
+
+function* combinations(pitches, initial) {
+  if (pitches.length == 0) {
+    yield initial;
+  } else {
+    for (const pitch of Pitch.listPitchClass(pitches[0])) {
+      yield* combinations(pitches.slice(1), [...initial, pitch]);
+    }
+  }
+}
+
+function argMax(it, fn) {
+  it = it[Symbol.iterator]();
+  const first = it.next().value;
+  let max = fn(first);
+  let values = [first];
+  for (const x of it) {
+    const m = fn(x);
+    if (m == max) {
+      values.push(x);
+    } else if (m > max) {
+      max = m;
+      values = [x];
+    }
+  }
+  return values;
+}
+
+function argMin(it, fn) {
+  return argMax(it, x => -fn(x));
+}
+
+function uniqueLetters(N) {
+  let sum = 0;
+  for (let i = 0; i < N.length - 1; i++) {
+    for (let j = i + 1; j < N.length; j++) {
+      if (N[i].getLetter() === N[j].getLetter()) {
+        sum++;
+      }
+    }
+  }
+  return N.length - sum;
+}
+
+function augmentedDimished(N) {
+  let sum = 0;
+  for (let i = 0; i < N.length - 1; i++) {
+    for (let j = i + 1; j < N.length; j++) {
+      const ci = N[i].getLetter();
+      const cj = N[j].getLetter();
+      const dc = ci >= cj ? ci - cj : ci - cj + 7;
+      const si = N[i].getPitchClass();
+      const sj = N[j].getPitchClass();
+      const ds = si >= sj ? si - sj : si - sj + 12;
+      if (dc == 0) {
+        sum += 1;
+      } else if ((dc == 1 || dc == 2) && ds > 2 * dc) {
+        sum += ds - 2 * dc;
+      } else if (
+        (dc == 1 || dc == 2 || dc == 3 || dc == 4) &&
+        ds < 2 * dc - 1
+      ) {
+        sum += 2 * dc - 1 - ds;
+      } else if (
+        (dc == 3 || dc == 4 || dc == 5 || dc == 6) &&
+        ds > 2 * dc - 1
+      ) {
+        sum += ds - 2 * dc + 1;
+      } else if ((dc == 5 || dc == 6) && ds < 2 * dc - 2) {
+        sum += 2 * dc - 2 - ds;
+      }
+    }
+  }
+  return sum;
+}
+
+function naturals(N) {
+  return N.filter(n => n.isNatural()).length;
+}
+
+function doubles(N) {
+  return N.filter(n => n.isDoubleFlat() || n.isDoubleSharp()).length;
+}
+
+function between(N) {
+  let sum = 0;
+  for (let i = 0; i < N.length; i++) {
+    if (
+      N[i].isNatural() &&
+      N[(i + 2) % N.length].isNatural() &&
+      N[(i + 2) % N.length].getLetter() - N[i].getLetter() === 1 &&
+      N[(i + 1) % N.length].isSharp()
+    ) {
+      sum++;
+    }
+  }
+  return sum;
+}
+
+export function spellScale(scale, root) {
+  let candidates = combinations(
+    scale.map(pitchClass => (root.getPitchClass() + pitchClass) % 12),
+    [root]
+  );
+
+  // phase 1
+  candidates = argMax(candidates, uniqueLetters);
+  if (candidates.length === 1) {
+    return candidates[0];
+  }
+
+  // phase 2
+  candidates = argMin(candidates, augmentedDimished);
+  if (candidates.length === 1) {
+    return candidates[0];
+  }
+
+  // phase 3
+  candidates = argMax(candidates, naturals);
+  if (candidates.length === 1) {
+    return candidates[0];
+  }
+
+  // phase 4
+  candidates = argMin(candidates, doubles);
+  if (candidates.length === 1) {
+    return candidates[0];
+  }
+
+  // phase 5
+  candidates = argMax(candidates, between);
+  return candidates[0];
+}
