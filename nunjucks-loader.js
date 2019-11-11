c1f0e5e968f47e2e16bfa180e744e0ab7e264d87
Tuomas Siipola <tuomas@zpl.fi>
2019-11-11 14:35:34 +0200

    Refactor nunjucks-loader

--- nunjucks-loader.js
+++ nunjucks-loader.js
@@ -1,23 +1,27 @@
 // SPDX-FileCopyrightText: 2019 Tuomas Siipola
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-const loaderUtils = require('loader-utils');
 const nunjucks = require('nunjucks');
+const SafeString = nunjucks.runtime.SafeString;
 const path = require('path');
 const fs = require('fs');
 
-const requireBegin = new RegExp('__REQUIRE_BEGIN_GJRATYQZ__', 'g');
-const requireEnd = new RegExp('__REQUIRE_END_HTMZXRXG__', 'g');
+// Support `require` by first inserting markers around the module name in the
+// rendered template and later replacing them by an actual call to `require`.
+const requireBegin = '__REQUIRE_BEGIN_GJRATYQZ__';
+const requireEnd = '__REQUIRE_END_HTMZXRXG__';
+const requireRegExp = new RegExp(requireBegin + '(.*?)' + requireEnd, 'g');
 
+// Custom Nunjucks loader which loads templates from the file system, supports
+// relative paths and adds dependencies to webpack.
 class MyLoader {
-  constructor(context, addDependency) {
-    this.context = context;
-    this.addDependency = addDependency;
+  constructor(webpackLoader) {
+    this.webpackLoader = webpackLoader;
   }
 
   getSource(name) {
-    const fullpath = path.resolve(this.context, name);
-    this.addDependency(fullpath);
+    const fullpath = path.resolve(this.webpackLoader.context, name);
+    this.webpackLoader.addDependency(fullpath);
     return {
       src: fs.readFileSync(fullpath, 'utf-8'),
       path: fullpath,
@@ -26,18 +30,13 @@ class MyLoader {
 }
 
 module.exports = function(content) {
-  const options = loaderUtils.getOptions(this);
-  const env = new nunjucks.Environment(
-    new MyLoader(this.context, this.addDependency)
-  );
+  const env = new nunjucks.Environment(new MyLoader(this));
   const html = env.renderString(content, {
-    require: url =>
-      new nunjucks.runtime.SafeString(
-        requireBegin.source + url + requireEnd.source
-      ),
+    require: module => new SafeString(requireBegin + module + requireEnd),
   });
-  const code = JSON.stringify(html)
-    .replace(requireBegin, '" + require("')
-    .replace(requireEnd, '") + "');
+  const code = JSON.stringify(html).replace(
+    requireRegExp,
+    '" + require("$1") + "'
+  );
   return 'module.exports = ' + code + ';';
 };
